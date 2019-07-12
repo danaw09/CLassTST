9527741a187931cd2f98559e6f3da095e4ff7270
dana <danaw09@gmail.com>
2019-07-12 17:56:06 -0500

    added another method and checking the test to see if they pass

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -8,21 +8,32 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void _AddToEmptyList_ValueGoesToIndexZero()
-        {
-            //arrange
-            CustomClass<int> customclass = new CustomClass<int>();
-            int expected = 10;
-            int actual;
-            //act
-            customclass.Add(10);
-            actual = customclass[0];
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
+        //[TestMethod]
+        //public void Add_AddToEmptyList_ValueGoesToIndexZero()
+        //{
+        //    //arrange
+        //    CustomClass<int> customclass = new CustomClass<int>();
+        //    int[] inputValue = new int[] { };
 
-      
+        //    //assert
+        //    customclass.Add(inputValue)
+
+        //    Assert.AreSame(customclass[0], inputValue)
+        //}
+
+        [TestMethod]
+        public void Remove_An_Object()
+        {
+           // Arrange
+            CustomClass<string> list = new CustomClass<string>();
+            list.Add("string1");
+            list.Add("string2");
+            list.Add("string3");
+           // act
+            list.Remove("string1");
+           // Assert
+            Assert.AreEqual(list.size, 2);
+        }
         [TestMethod]
         public void Remove_Another_Object()
         {
