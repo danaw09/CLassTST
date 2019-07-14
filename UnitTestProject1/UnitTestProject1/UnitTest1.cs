a0e58980a1d1e8ed21d783f937b3a060d248c1ad
dana <danaw09@gmail.com>
2019-07-14 14:47:12 -0500

    added unit test plus operator and minus operator

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -21,19 +21,19 @@ namespace UnitTestProject1
         //    Assert.AreSame(customclass[0], inputValue)
         //}
 
-        [TestMethod]
-        public void Remove_An_Object()
-        {
-           // Arrange
-            CustomClass<string> list = new CustomClass<string>();
-            list.Add("string1");
-            list.Add("string2");
-            list.Add("string3");
-           // act
-            list.Remove("string1");
-           // Assert
-            Assert.AreEqual(list.size, 2);
-        }
+        //[TestMethod]
+        //public void Remove_An_Object()
+        //{
+        //   // Arrange
+        //    CustomClass<string> list = new CustomClass<string>();
+        //    list.Add("string1");
+        //    list.Add("string2");
+        //    list.Add("string3");
+        //   // act
+        //    list.Remove("string1");
+        //   // Assert
+        //    Assert.AreEqual(list.size, 2);
+        //}
         [TestMethod]
         public void Remove_Another_Object()
         {
@@ -134,7 +134,7 @@ namespace UnitTestProject1
             //Arrange
             CustomClass<string> list = new CustomClass<string>() { "string1", "string2" };
             CustomClass<string> secondList = new CustomClass<string>() { "string3", "string4" };
-            CustomClass<string>  actual = new CustomClass<string> () { "string1", "string2", "string3", "string4" };
+            CustomClass<string>  actual = new CustomClass<string> () { "string1", "string3", "string2", "string4" };
             //Act
             CustomClass<string> result = list.Zip(list, secondList);
             bool compare = true;
@@ -147,8 +147,49 @@ namespace UnitTestProject1
             }
             //Assert
             Assert.IsTrue(compare);
+
         }
 
+
+
+
+        [TestMethod]
+        public void Overloading_PlusOperator()
+        {
+
+        }
+
+
+
+
+        [TestMethod]
+        public void Overloading_MinusOperator()
+        {
+            //Arrange
+            CustomClass<string> list = new CustomClass<string>() { "one", "two" };
+            CustomClass<string>  second = new CustomClass<string>() { "three", "four" };
+            CustomClass<string> actualResult = new CustomClass<string>() { "one", "two" };
+            CustomClass<string> combinedList = (list + second);
+            bool compare = true;
+            for (int i = 0; i < list.size; i++)
+            {
+                if (list.objects[i] != actualResult.objects[i])
+                {
+                    compare = false;
+                }
+            }
+            //Act
+            CustomClass<string> result = (combinedList - second);
+            //Assert
+            Assert.IsTrue(compare);
+
+
+
+
+
+
+
+        }
 
 
 
